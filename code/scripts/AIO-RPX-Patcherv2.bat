cc0829c2ea81da05552825975bfd5faaf038e254
Torphedo <torphedo.launcher@gmail.com>
2024-05-31 01:13:05 -0400

    RPX Patcher: Fix dead XDelta link

--- code/scripts/AIO-RPX-Patcherv2.bat
+++ code/scripts/AIO-RPX-Patcherv2.bat
@@ -7,7 +7,7 @@ for /f "tokens=1,* delims=:" %%A in ('curl -ks https://api.github.com/repos/0CBH
 powershell -command "Expand-Archive wiiurpxtool.zip ."
 del wiiurpxtool.zip
 wiiurpxtool -d U-King.rpx PatchedRPX\U-King-decompressed.rpx
-curl -s -o xdelta.exe https://raw.githubusercontent.com/marco-calautti/DeltaPatcher/master/xdelta.exe
+curl -s -o xdelta.exe https://github.com/marco-calautti/DeltaPatcher/raw/b533ed219d4587b9fdf58885e5e986b4a340c38c/xdelta.exe
 for %%x in (%*) do (
    xdelta -d -n -s PatchedRPX\U-King-decompressed.rpx "%%~x" PatchedRPX\U-King-decompressed-patched.rpx
    del PatchedRPX\U-King-decompressed.rpx
